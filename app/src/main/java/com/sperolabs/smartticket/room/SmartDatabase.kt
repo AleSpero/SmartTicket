c1c7d00544e632610559dce62be05747f92536b9
Alessandro Sperotti <alessandro.sperotti@dsgroup.it>
2018-10-29 19:37:58 +0100

    added databasedatasource + init db

--- app/src/main/java/com/sperolabs/smartticket/room/SmartDatabase.kt
+++ app/src/main/java/com/sperolabs/smartticket/room/SmartDatabase.kt
@@ -1,8 +1,11 @@
 package com.sperolabs.smartticket.room
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.sperolabs.smartticket.models.Product
 import com.sperolabs.smartticket.models.ShoppingItem
+import com.sperolabs.smartticket.room.dao.ProductDao
+import com.sperolabs.smartticket.room.dao.ShoppingItemDao
 
 /**
  * Created by alessandros on 29/10/2018.
@@ -10,5 +13,9 @@ import com.sperolabs.smartticket.models.ShoppingItem
  */
 
 @Database(version = 1, entities = [Product::class, ShoppingItem::class])
-class SmartDatabase {
+abstract class SmartDatabase : RoomDatabase(){
+
+    abstract fun productDao() : ProductDao
+    abstract fun shoppingItemDao() : ShoppingItemDao
+
 }
